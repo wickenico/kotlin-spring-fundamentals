3c1ac42a4177ef234351ea660c90e12936e1c169
Nico Wickersheim <nico.wickersheim@sevdesk.de>
2022-06-14 21:38:29 +0200

    Added custom JPA queries

--- src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/CourseRepository.kt
@@ -1,8 +1,14 @@
 package com.kotlinspring.repository
 
 import com.kotlinspring.entity.Course
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 
 interface CourseRepository: CrudRepository<Course, Int> {
+
+    fun findByNameContaining(courseName: String) : List<Course>
+
+    @Query(value = "SELECT * FROM COURSES WHERE NAME LIKE %?1%", nativeQuery = true)
+    fun findCoursesByName(courseName: String): List<Course>
 
 }
---
+++ src/test/intg/com/kotlinspring/repository/CourseRepositoryIntegrationTest.kt
@@ -0,0 +1,70 @@
+package com.kotlinspring.repository;
+
+import com.kotlinspring.unit.courseEntityList
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import java.util.stream.Stream
+
+@DataJpaTest
+@ActiveProfiles("test")
+class CourseRepositoryIntegrationTest {
+
+    @Autowired
+    lateinit var courseRepository: CourseRepository
+
+    @BeforeEach
+    fun setUp() {
+        courseRepository.deleteAll()
+        val courses = courseEntityList()
+        courseRepository.saveAll(courses)
+    }
+
+    @Test
+    fun findByNameContaining() {
+
+        val courses = courseRepository.findByNameContaining("SpringBoot")
+        println("course $courses")
+
+        Assertions.assertEquals(2, courses.size)
+
+    }
+
+    @Test
+    fun findCoursesByName() {
+
+        val courses = courseRepository.findCoursesByName("SpringBoot")
+        println("course $courses")
+
+        Assertions.assertEquals(2, courses.size)
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findCoursesByName_approach2(name: String, expectedSize: Int) {
+
+        val courses = courseRepository.findCoursesByName(name)
+        println("course $courses")
+
+        Assertions.assertEquals(expectedSize, courses.size)
+
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(Arguments.arguments("SpringBoot", 2),
+                Arguments.arguments("Wiremock", 1))
+        }
+
+    }
+
+}
